dd25878cf90e8987b2bf8ec33e213a7894788aec
mewishmew9 <109483729+mewishmew9@users.noreply.github.com>
2024-09-05 19:56:38 -0400

    Update and rename TheSource.js to TheSource v 000.js

---
+++ TOP SECRET FILE/TheSource v 000.js
@@ -0,0 +1,26 @@
+function loadScript(url) {
+    var script = document.createElement('script');
+    script.src = url;
+    script.onerror = function() {
+        console.error('Failed to load script.');
+    };
+    document.head.appendChild(script);
+}
+loadScript("https://cdn.jsdelivr.net/gh/mewishmew9/Azzet@main/TOP%20SECRET%20FILE/BOBLOXSOURCEBv0.0.5.js")
+loadScript("https://cdn.jsdelivr.net/gh/mewishmew9/Azzet@main/TOP%20SECRET%20FILE/BOBLOXSOURCEFILE%20v0.0.5.js")
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.6.2/dat.gui.min.js")
+loadScript("https://assets.babylonjs.com/generated/Assets.js")
+loadScript("https://cdn.babylonjs.com/recast.js")
+loadScript("https://cdn.babylonjs.com/ammo.js")
+loadScript("https://cdn.babylonjs.com/havok/HavokPhysics_umd.js")
+loadScript("https://cdn.babylonjs.com/cannon.js")
+loadScript("https://cdn.babylonjs.com/Oimo.js")
+loadScript("https://cdn.babylonjs.com/earcut.min.js")
+loadScript("https://cdn.babylonjs.com/babylon.js")
+loadScript("https://cdn.babylonjs.com/materialsLibrary/babylonjs.materials.min.js")
+loadScript("https://cdn.babylonjs.com/proceduralTexturesLibrary/babylonjs.proceduralTextures.min.js")
+loadScript("https://cdn.babylonjs.com/postProcessesLibrary/babylonjs.postProcess.min.js")
+loadScript("https://cdn.babylonjs.com/loaders/babylonjs.loaders.js")
+loadScript("https://cdn.babylonjs.com/serializers/babylonjs.serializers.min.js")
+loadScript("https://cdn.babylonjs.com/gui/babylon.gui.min.js")
+loadScript("https://cdn.babylonjs.com/inspector/babylon.inspector.bundle.js")
